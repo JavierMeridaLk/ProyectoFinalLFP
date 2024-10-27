51c6b719c42a8e07019946399b6bff5187d2894c
Javier Mérida <javiermerida202230638@cunoc.edu.gt>
2024-10-27 01:51:35 -0600

    Diseño fronted

--- ProyectoFinal/src/main/java/proyectofinal/proyectofinal/ProyectoFinal.java
+++ ProyectoFinal/src/main/java/proyectofinal/proyectofinal/ProyectoFinal.java
@@ -4,6 +4,8 @@
 
 package proyectofinal.proyectofinal;
 
+import fronted.FramePrincipal;
+
 /**
  *
  * @author xavi
@@ -11,6 +13,7 @@ package proyectofinal.proyectofinal;
 public class ProyectoFinal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        FramePrincipal frame = new FramePrincipal();
+        frame.setVisible(true);
     }
 }
